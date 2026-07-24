7d652a00f636eba160c8f07ee0ffc4301a5e89fc
Peterson Tenorio <petersontenorio10@gmail.com>
2026-07-24 14:27:41 -0300

    feat: add Lombok

--- src/main/java/com/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package com.java10x.CadastroDeNinjas.Missoes;
 
 import com.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
@@ -17,45 +23,4 @@ public class MissoesModel {
     // Uma missao pode ter varos ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninja;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nomeMissao, String dificuldade,  List<NinjaModel> ninja) {
-        this.nomeMissao = nomeMissao;
-        this.dificuldade = dificuldade;
-        this.ninja = ninja;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-
-    public String getNomeMissao() {
-        return nomeMissao;
-    }
-
-    public void setNomeMissao(String nomeMissao) {
-        this.nomeMissao = nomeMissao;
-    }
-
-    public String getDificuldade() {
-        return dificuldade;
-    }
-
-    public void setDificuldade(String dificuldade) {
-        this.dificuldade = dificuldade;
-    }
-
-    public List<NinjaModel> getNinja() {
-        {
-            return ninja;
-        }
-    }
-
-    public void setNinja(List<NinjaModel> ninja) {
-            this.ninja = ninja;
-        }
-
 }
--- src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,9 +2,13 @@ package com.java10x.CadastroDeNinjas.Ninjas;
 
 import com.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "tb_cadastro_ninjas")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
     @Id
@@ -17,37 +21,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id") // -> chave estrangeira (Foreing Key)
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
