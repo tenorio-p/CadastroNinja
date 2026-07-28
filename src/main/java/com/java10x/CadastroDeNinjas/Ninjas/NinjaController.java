123fb005993514e2dca45d393dc2d541280d1e43
Peterson Tenorio <petersontenorio10@gmail.com>
2026-07-28 15:35:33 -0300

    feat: update ninja controller

--- src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package com.java10x.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,6 +9,37 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Olá mundo! Sejam bem vindos.";
+    }
+
+    // Adicionar Ninja
+    @PostMapping("/criar")
+    public String criarNinja(){
+        return "Ninja criado com sucesso!";
+    }
+
+
+    // Mostrar todos os ninjas
+    @GetMapping("/todos")
+    public String mostrarNinjas(){
+        return "Todos os ninjas cadastrados.";
+    }
+
+    // Mostrar ninja por id
+    @GetMapping("/todosID")
+    public String mostrarNinjaPorId(){
+        return "Ninja por id";
+    }
+
+    // Alterar dados do ninja
+    @PutMapping("/alterarID")
+    public String alterarNinja(){
+        return "Ninja com ID atualizado.";
+    }
+
+    // Deletar ninja
+    @DeleteMapping("/deletarID")
+    public String deletarNinja(){
+        return "Ninja com ID deletado.";
     }
 
 }
