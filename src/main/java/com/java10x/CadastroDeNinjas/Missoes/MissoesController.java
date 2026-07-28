5b4f01ee4b8d7f67f2583f7092a3fb316fab0a07
Peterson Tenorio <petersontenorio10@gmail.com>
2026-07-28 15:48:18 -0300

    feat: add missoesController

--- src/main/java/com/java10x/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,29 @@
 package com.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesController {
+
+    // Mostrar as missoes
+    @GetMapping("/mostrar")
+    public String listarMissoes(){
+        return "Lista de missoes";
+    }
+    // Criar uma missao
+    @PostMapping("/criar")
+    public String criarMissao(){
+        return "Missao criada";
+    }
+    // Alterar uma missao
+    @PutMapping("/alterar")
+    public String alterarMissao() {
+        return "Missao alterada";
+    }
+    // Deletar uma missao
+    @DeleteMapping("/deletar")
+    public String deletarMissao() {
+        return "Missao deletada";
+    }
 }
