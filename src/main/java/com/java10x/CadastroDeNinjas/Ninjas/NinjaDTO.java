bd31f0d609a0b6678f41f83aee3c148e6450d631
Peterson Tenorio <petersontenorio10@gmail.com>
2026-07-30 15:30:04 -0300

    chore: include DTO and Mapper

--- src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/com/java10x/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,21 @@
 package com.java10x.CadastroDeNinjas.Ninjas;
 
+import com.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class NinjaDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+    private String rank;
+    private MissoesModel missoes;
+
+
 }
